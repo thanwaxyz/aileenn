63de44a81c7e7f00d5481302a7c7f64fcf8f3a63
thanwaxyz <thanwaxyz@gmail.com>
2025-03-04 14:13:28 +0000

    add new files

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+
+console.log("javascript is working")
+
+
+function setup() {
+    createCanvas(500, 500)
+}
+
+
+
+function setup() {
+    createCanvas(100, 100);
+  
+    
+    // Get the current minute.
+    let m = minute();
+  
+    // Style the text.
+    textAlign(LEFT, CENTER);
+    textSize(18);
+    textFont('Courier New');
+  
+    // Display the minute.
+    text(`Current minute: ${m}`, 10, 50, 80);
+  
+    describe(`The text 'Current minute: ${m}' written in black on a gray background.`);
+  }
+
+
+
+
+    
